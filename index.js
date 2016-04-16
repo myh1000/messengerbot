73c1ec93732a8e125ce102995171c56d7da4a142
myh1000 <myh1000@gmail.com>
2016-04-15 19:53:03 -0700

    --allow-empty-message

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
-var os = require('os');
+var os = require('os')
+var math = require('mathjs')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
@@ -51,6 +52,10 @@ app.post('/webhook/', function (req, res) {
                 }
                 if (text === 'dank') {
                   sendTextMessage(sender, "memes")
+                  continue
+                }
+                if (getFirstWord(text) === 'math') {
+                  sendTextMessage(sender, math.eval(text.substr(event.message.text.indexOf(" ") + 1)))
                   continue
                 }
                 sendTextMessage(sender, text.substring(0, 200) + ": not same")
